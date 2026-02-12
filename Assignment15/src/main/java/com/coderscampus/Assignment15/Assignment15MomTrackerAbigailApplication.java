296976866a4bbef787b9517955c02058123ac287
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2026-02-12 11:00:58 -0600

    changed the imports

--- Assignment15/src/main/java/com/coderscampus/Assignment15/Assignment15MomTrackerAbigailApplication.java
+++ Assignment15/src/main/java/com/coderscampus/Assignment15/Assignment15MomTrackerAbigailApplication.java
@@ -1,6 +1,5 @@
 package com.coderscampus.Assignment15;
 
-import com.coderscampus.Assignment15.config.DatabasePlatformInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Assignment15MomTrackerAbigailApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Assignment15MomTrackerAbigailApplication.class);
-		app.addInitializers(new DatabasePlatformInitializer());
-		app.run(args);
+		SpringApplication.run(Assignment15MomTrackerAbigailApplication.class, args);
 	}
 
 }
